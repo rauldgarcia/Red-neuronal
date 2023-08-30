004c23a725711198920322ba9f1062564bfca2dd
Raul Garcia <rauld.garcia95@gmail.com>
2023-08-29 18:09:45 -0600

    Relu and Softrelu function is added

---
+++ relu.py
@@ -0,0 +1,20 @@
+import math
+import matplotlib.pyplot as plt
+
+def softrelu(num):
+    return math.log(1 + (math.e**num))
+
+def relu(num):
+    return max(0,num)
+
+listx = []
+listy = []
+listy2= []
+for x in range(-10, 10):
+    listx.append([x])
+    listy.append([softrelu(x)])
+    listy2.append([relu(x)])
+
+plt.plot(listx, listy)
+plt.plot(listx, listy2)
+plt.show()
