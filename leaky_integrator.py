f7e0da139d485240486f79102f4223b5067f28cb
Raul Garcia <rauld.garcia95@gmail.com>
2023-09-08 19:17:34 -0600

    new input signal is added

--- leaky_integrator.py
+++ leaky_integrator.py
@@ -1,17 +1,38 @@
 import matplotlib.pyplot as plt
 import math
+import numpy as np
+import copy
 
-a = 1 # leakage coefficient
-b = 1 # input scaling
-c = 0 # inhibitory feedback gain
+def relu(num):
+    return max(0, num)
+
+def softrelu(num):
+    return math.log(1 + (math.e**num))
+
+a = 1 # leakage coefficient >= 0
+b = 0.1 # input scaling > 0
+c = 0.7 # inhibitory feedback gain >= 0
+t = 1 # time delay > 0
 
 listx = []
 listy = []
-t = 0
-for x in range (0, 11):
-    listx.append([x])
-    listy.append([(k * (math.e ** (-a*t))) + (x/a)])
-    t += 1
+listy2 = []
+time = 0
+last_y = 0
+listx.append([0])
+listy.append([0])
+listy2.append([0])
+
+
+for signal in range(0, 101):
+    listx.append([time])
+    input_signal = np.sin(0.1 * signal) + 0.5 * np.sin(0.5 * signal)
+    new_y = -(a*input_signal) + (b*time) - (c*last_y)
+    listy.append([new_y])
+    listy2.append([input_signal])
+    last_y = copy.deepcopy(new_y)
+    time += t
 
 plt.plot(listx, listy)
+plt.plot(listx, listy2)
 plt.show()
