1ca81a70fa389c9cfaad94ee09819b63871ad36b
Raul Garcia <rauld.garcia95@gmail.com>
2023-09-15 17:36:00 -0600

    Backward pass in loss categorical cross entropy

--- dense_layer.py
+++ dense_layer.py
@@ -110,6 +110,24 @@ class Loss_CategoricalCrossentropy(Loss):
         # Losses
         negative_log_likelihoods = -np.log(correct_confidences)
         return negative_log_likelihoods
+    
+    # Backward pass
+    def backward(self, dvalues, y_true):
+
+        # Number of samples
+        samples = len(dvalues)
+        # Number of labels in every sample
+        # We'll use the first sample to count them
+        labels = len(dvalues[0])
+
+        # If labels are sparse, turn them into one-hot vector
+        if len(y_true.shape) == 1:
+            y_true = np.eye(labels)[y_true]
+
+        # Calculate gradient
+        self.dinputs = -y_true / dvalues
+        # Normalize gradient
+        self.dinputs = self.dinputs / samples
 
 
 # Create dataset
