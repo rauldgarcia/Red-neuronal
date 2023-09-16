67e50c128730bcfc011a7889658d928302a39846
Raul Garcia <rauld.garcia95@gmail.com>
2023-09-16 12:35:08 -0600

    Softmax derivative is added

--- dense_layer.py
+++ dense_layer.py
@@ -61,6 +61,22 @@ class Activation_Softmax:
 
         self.output = probabilities
 
+    # Backward pass
+    def backward(self, dvalues):
+
+        # Create unitialized array
+        self.dinputs = np.empty_like(dvalues)
+        
+        # Enumerate outputs and gradients
+        for index, (single_output, single_dvalues) in enumerate(zip(self.output, dvalues)):
+            # Flatten output array
+            single_output = single_output.reshape(-1, 1)
+            # Calculate Jacobian matrix of the output and
+            jacobian_matrix = np.diagflat(single_output) - np.dot(single_output, single_output.T)
+            # Calculate sample-wise gradient
+            # and add it to the array of sample gradients
+            self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
+
 
 # Common loss class
 class Loss:
