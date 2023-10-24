6bc09261a8c7840cd36eba97f160720f634f1c9f
Raul Garcia <rauld.garcia95@gmail.com>
2023-10-23 19:53:46 -0600

    data preparation is added

---
+++ data_load.py
@@ -0,0 +1,48 @@
+import numpy as np
+import cv2
+import os
+
+# Loads a MNIST dataset
+def load_mnist_dataset(dataset, path):
+
+    # Scan all the directiories and create a list of labels
+    labels = os.listdir(os.path.join(path, dataset))
+
+    # Create list for samples and labels
+    x = []
+    y = []
+
+    # For each label folder
+    for label in labels:
+        # And for each image in given folder
+        for file in os.listdir(os.path.join(path, dataset, label)):
+            # Read the image
+            image = cv2.imread(os.path.join(path, dataset, label, file), cv2.IMREAD_UNCHANGED)
+
+            # And append it and a label to the lists
+            x.append(image)
+            y.append(label)
+
+    # Convert the data to proper numpy arrays and return
+    return np.array(x), np.array(y).astype('uint8')
+
+# MNIST dataset (train + test)
+def create_data_mnist(path):
+
+    # Load both sets separately
+    x, y = load_mnist_dataset('train', path)
+    x_test, y_test = load_mnist_dataset('test', path)
+
+    # And return all the data
+    return x, y, x_test, y_test
+
+# Create dataset
+x, y, x_test, y_test = create_data_mnist('fashion_mnist_images')
+
+# Scale features
+x = (x.astype(np.float32) - 127.5) / 127.5
+x_test = (x.astype(np.float32) - 127.5) / 127.5
+
+# Reshape to vectors
+x = x.reshape(x.shape[0], -1)
+x_test = x_test.reshape(x_test.shape[0], -1)
