f310616c3adda96709675f92c1a948e6c9d230c1
Raul Garcia <rauld.garcia95@gmail.com>
2023-10-24 17:37:42 -0600

    suffle is added

--- data_load.py
+++ data_load.py
@@ -1,6 +1,8 @@
 import numpy as np
 import cv2
 import os
+import nnfs
+import matplotlib.pyplot as plt
 
 # Loads a MNIST dataset
 def load_mnist_dataset(dataset, path):
@@ -36,6 +38,8 @@ def create_data_mnist(path):
     # And return all the data
     return x, y, x_test, y_test
 
+nnfs.init()
+
 # Create dataset
 x, y, x_test, y_test = create_data_mnist('fashion_mnist_images')
 
@@ -46,3 +50,14 @@ x_test = (x.astype(np.float32) - 127.5) / 127.5
 # Reshape to vectors
 x = x.reshape(x.shape[0], -1)
 x_test = x_test.reshape(x_test.shape[0], -1)
+
+keys = np.array(range(x.shape[0]))
+
+np.random.shuffle(keys)
+x = x[keys]
+y = y[keys]
+
+plt.imshow((x[4].reshape(28, 28)))
+plt.show()
+
+print(y[4])
